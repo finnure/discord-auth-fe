7d027f5442a36956d2388c529ef64fc8d20a9477
finnure <finnur.eiriksson@klappir.com>
2022-03-28 23:21:07 +0000

    Share on github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,90 @@
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpParams,
+} from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { catchError, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'klauvi-auth';
+  userFound = false;
+  username: string;
+  error = 0;
+  form = this.fb.group({
+    username: '',
+    password: '',
+  });
+
+  submit() {
+    if (this.userFound) {
+      this.finish();
+    } else {
+      this.login();
+    }
+  }
+
+  login() {
+    const { username } = this.form.value;
+    this.http
+      .get(`/login?username=${username}`)
+      .pipe(
+        tap(() => {
+          this.userFound = true;
+          this.username = this.form.value.username;
+          this.error = 0;
+        }),
+        catchError((error: HttpErrorResponse) => {
+          this.form.reset();
+          if (error.status === 0) {
+            this.error = 500;
+            return error.message;
+          } else {
+            this.error = error.status;
+            return error.message;
+          }
+        })
+      )
+      .subscribe();
+  }
+
+  finish() {
+    const { username, password } = this.form.value;
+    this.http
+      .get(`/auth?username=${username}&password=${password}`)
+      .pipe(
+        tap(() => {
+          const url = window.location.href;
+          this.error = 0;
+          if (url.includes('?')) {
+            const httpParams = new HttpParams({
+              fromString: url.split('?')[1],
+            });
+            const replace = httpParams.get('url');
+            if (replace) {
+              window.location.replace(replace);
+            }
+          }
+        }),
+        catchError((error: HttpErrorResponse) => {
+          this.userFound = false;
+          this.form.reset();
+          if (error.status === 0) {
+            this.error = 500;
+            return error.message;
+          } else {
+            this.error = error.status;
+            return error.message;
+          }
+        })
+      )
+      .subscribe();
+  }
+
+  constructor(private fb: FormBuilder, private http: HttpClient) {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
